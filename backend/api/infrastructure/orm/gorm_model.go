91f0926751fc03efad0b4f94de41398e12486aa3
yoshimi-I <iwatsuki.443@outlook.com>
2023-10-15 04:10:01 +0900

    fix: モデルのテーブル名修正

--- backend/api/infrastructure/orm/gorm_model.go
+++ backend/api/infrastructure/orm/gorm_model.go
@@ -1,1 +1,58 @@
 package orm
+
+import (
+	"time"
+)
+
+// Users corresponds to the Users table in the database.
+type Users struct {
+	UserID     uint         `gorm:"primaryKey;autoIncrement"`
+	Name       string       `gorm:"type:varchar(255)"`
+	Email      string       `gorm:"type:varchar(255);unique"`
+	CreatedAt  time.Time    `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time    `gorm:"autoUpdateTime"`
+	Activities []Activities `gorm:"foreignKey:UserID"`
+}
+
+// Activities corresponds to the Activities table in the database.
+type Activities struct {
+	ActivityID   uint `gorm:"primaryKey;autoIncrement"`
+	UserID       uint
+	ActivityDate time.Time
+	Notes        string          `gorm:"type:text"`
+	CreatedAt    time.Time       `gorm:"autoCreateTime"`
+	UpdatedAt    time.Time       `gorm:"autoUpdateTime"`
+	Plays        []Play          `gorm:"foreignKey:ActivityID"`
+	Studies      []StudyStartEnd `gorm:"foreignKey:ActivityID"`
+	Breaks       []BreakStartEnd `gorm:"foreignKey:ActivityID"`
+}
+
+// Play corresponds to the Play table in the database.
+type Play struct {
+	PlayID     uint `gorm:"primaryKey;autoIncrement"`
+	ActivityID uint
+	StartTime  time.Time
+	EndTime    time.Time
+	CreatedAt  time.Time `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
+}
+
+// StudyStartEnd corresponds to the StudyStartEnd table in the database.
+type StudyStartEnd struct {
+	StudyID    uint `gorm:"primaryKey;autoIncrement"`
+	ActivityID uint
+	StartTime  time.Time
+	EndTime    time.Time
+	CreatedAt  time.Time `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
+}
+
+// BreakStartEnd corresponds to the BreakStartEnd table in the database.
+type BreakStartEnd struct {
+	BreakID    uint `gorm:"primaryKey;autoIncrement"`
+	ActivityID uint
+	StartTime  time.Time
+	EndTime    time.Time
+	CreatedAt  time.Time `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
+}
