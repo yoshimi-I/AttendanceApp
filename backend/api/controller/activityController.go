838de3288452978a67529486045e6f723ce9f5ff
yoshimi-I <iwatsuki.443@outlook.com>
2023-10-15 22:02:07 +0900

    add: とりあえずcontrollerの雛形作成

--- backend/api/controller/activityController.go
+++ backend/api/controller/activityController.go
@@ -1,1 +1,35 @@
 package controller
+
+import (
+	"github.com/yoshimi-I/AttendanceApp/usecase"
+	"net/http"
+)
+
+type ActivityController interface {
+	AddActivity() http.HandlerFunc
+	UpdateActivity() http.HandlerFunc
+	DeleteActivity() http.HandlerFunc
+}
+
+type ActivityControllerImpl struct {
+	ActivityUsecase usecase.ActivityUsecase
+}
+
+func (a ActivityControllerImpl) AddActivity() http.HandlerFunc {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a ActivityControllerImpl) UpdateActivity() http.HandlerFunc {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a ActivityControllerImpl) DeleteActivity() http.HandlerFunc {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewActivityController(au usecase.ActivityUsecase) ActivityController {
+	return &ActivityControllerImpl{ActivityUsecase: au}
+}
--- backend/api/controller/historyController.go
+++ backend/api/controller/historyController.go
@@ -1,1 +1,32 @@
 package controller
+
+import (
+	"github.com/yoshimi-I/AttendanceApp/usecase"
+	"net/http"
+)
+
+type HistoryController interface {
+	GetAllHistory() http.HandlerFunc
+	GetHistoryByDate() http.HandlerFunc
+}
+
+type HistoryControllerImpl struct {
+	// ここで使うusecaseを全て実装
+	historyUsecasse usecase.HistoryUsecase
+}
+
+func (h HistoryControllerImpl) GetAllHistory() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		return
+	}
+}
+
+func (h HistoryControllerImpl) GetHistoryByDate() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		return
+	}
+}
+
+func NewHisoryController(hu usecase.HistoryUsecase) HistoryController {
+	return &HistoryControllerImpl{historyUsecasse: hu}
+}
