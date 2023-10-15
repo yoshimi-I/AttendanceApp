5db8dc1d02220915c43fdf10f1bcd57038f1d3b9
yoshimi-I <iwatsuki.443@outlook.com>
2023-10-15 22:02:36 +0900

    create: Repositoryの雛形作成

--- backend/api/domain/repository/activityRepository.go
+++ backend/api/domain/repository/activityRepository.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"time"
+)
+
+type Activity struct {
+	Type      string
+	Timestamp time.Time
+}
+
+type ActivtyRepository interface {
+	PostStudyActivity(activity Activity) error
+	PutStudyActivity(id int, activity Activity) error
+	DeleteStudyActivity(id int) error
+}
--- backend/api/infrastructure/repository/activityRepositoryImpl.go
+++ backend/api/infrastructure/repository/activityRepositoryImpl.go
@@ -1,1 +1,26 @@
 package repository
+
+import (
+	"github.com/yoshimi-I/AttendanceApp/domain/repository"
+	"gorm.io/gorm"
+)
+
+type ActivityRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func (a ActivityRepositoryImpl) PostStudyActivity(activity repository.Activity) error {
+	return nil
+}
+
+func (a ActivityRepositoryImpl) PutStudyActivity(id int, activity repository.Activity) error {
+	return nil
+}
+
+func (a ActivityRepositoryImpl) DeleteStudyActivity(id int) error {
+	return nil
+}
+
+func NewActivityRespotiroy(db *gorm.DB) repository.ActivtyRepository {
+	return &ActivityRepositoryImpl{db: db}
+}
