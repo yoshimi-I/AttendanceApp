2a1dbcd748a9c2a4cca34795acc42ce2ddf30818
yoshimi-I <iwatsuki.443@outlook.com>
2023-10-15 22:03:09 +0900

    create: usecaseの雛形実装

--- backend/api/usecase/activityUsecase.go
+++ backend/api/usecase/activityUsecase.go
@@ -1,1 +1,36 @@
 package usecase
+
+import (
+	"github.com/yoshimi-I/AttendanceApp/domain/repository"
+	"github.com/yoshimi-I/AttendanceApp/usecase/dto"
+)
+
+type ActivityUsecase interface {
+	// レスポンス値にDTOを使う
+	AddStudyActivity(activity dto.ActivityRequestDTO) (dto.ActivityResponseDTO, error)
+	UpdateStudyActivity(activity dto.ActivityRequestDTO) (dto.ActivityResponseDTO, error)
+	DeleteActivity(activityID int) error
+}
+
+type ActivityUsecaseImpl struct {
+	activityRepo repository.ActivtyRepository
+}
+
+func (a ActivityUsecaseImpl) AddStudyActivity(activity dto.ActivityRequestDTO) (dto.ActivityResponseDTO, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a ActivityUsecaseImpl) UpdateStudyActivity(activity dto.ActivityRequestDTO) (dto.ActivityResponseDTO, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a ActivityUsecaseImpl) DeleteActivity(activityID int) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewActivityUsecase(ar repository.ActivtyRepository) ActivityUsecase {
+	return &ActivityUsecaseImpl{activityRepo: ar}
+}
--- backend/api/usecase/historyUsecase.go
+++ backend/api/usecase/historyUsecase.go
@@ -1,19 +1,38 @@
 package usecase
 
 import (
+	"errors"
+	"github.com/yoshimi-I/AttendanceApp/domain/model"
 	"github.com/yoshimi-I/AttendanceApp/domain/repository"
-	"net/http"
+	"time"
 )
 
 // まずは扱う関数のinterfaceを実装
-type StudyHistoryUsecase interface {
-	getStudyActivity() http.HandlerFunc
+type HistoryUsecase interface {
+	getStudyActivity(date string) (*model.Activities, error)
+	getAllStudyActivity() (*[]model.Activities, error)
 }
 
-type StudyHistoryUsecaseImpl struct {
-	studyHistoryRepo repository.StudyHistoryRepository
+// 構造体を実装
+type HistoryUsecaseImpl struct {
+	studyHistoryRepo repository.HistoryRepository
 }
 
-func NewStudyHistoryUsecase(studyHistoryRepo repository.StudyHistoryRepoImpl) StudyHistoryUsecase {
-	return &StudyHistoryUsecaseImpl
+// 構造体にinterfaceの関数を実装
+func (h HistoryUsecaseImpl) getStudyActivity(date string) (*model.Activities, error) {
+	_, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return nil, errors.New("invalid date format")
+	}
+	return &model.Activities{}, nil
+}
+
+func (h HistoryUsecaseImpl) getAllStudyActivity() (*[]model.Activities, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+// 関数を実装した構造体をnewする関数を実装,またこのとき返り値はinterface
+func NewHistoryUsecase(hr repository.HistoryRepository) HistoryUsecase {
+	return &HistoryUsecaseImpl{studyHistoryRepo: hr}
 }
