e69f2cfdfd0780107a86d8886eeed761857cb2d6
yoshimi-I <iwatsuki.443@outlook.com>
2023-10-15 22:03:24 +0900

    create: DTO作成

--- backend/api/usecase/dto/activityResponseDto.go
+++ backend/api/usecase/dto/activityResponseDto.go
@@ -1,1 +1,15 @@
 package dto
+
+import "time"
+
+// ActivityRequestDTO アクティビティのリクエストデータ
+type ActivityRequestDTO struct {
+	Type      string    `json:"type"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// ActivityResponseDTO アクティビティのレスポンスデータ
+type ActivityResponseDTO struct {
+	Message      string `json:"message"`
+	AttendanceID int    `json:"attendanceId"`
+}
