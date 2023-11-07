c364b98f85537cdca0b2dbcc0b9b67e74916fa19
yoshimi-I <iwatsuki.443@outlook.com>
2023-11-08 01:59:06 +0900

    delete: いらんなぁということで...

--- db/migration/20231026162607_8_add_yaer_cloum.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE Attendances ADD year INT;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE Attendances DROP COLUMN year;
--- +goose StatementEnd
