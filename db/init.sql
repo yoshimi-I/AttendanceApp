72b4317518214a268e658f45c13a210972415d79
yoshimi-I <iwatsuki.443@outlook.com>
2023-11-08 01:59:30 +0900

    fix: テーブルを少し変えたので

--- db/init.sql
+++ db/init.sql
@@ -1,37 +1,62 @@
--- Usersテーブルの作成
+-- Usersテーブル
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       firebase_id VARCHAR(255) UNIQUE,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- Attendance_typesテーブルの作成
+-- 作業の種類を含めるテーブル
 CREATE TABLE attendance_types (
-    attendance_type_id INT PRIMARY KEY,
-    action_type VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                                  attendance_type_id INT PRIMARY KEY,
+                                  action_type VARCHAR(100) NOT NULL,
+                                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- Attendancesテーブルの作成
+-- Userの現在の状態の種類を管理するテーブル
+CREATE TABLE user_status_types (
+                                   user_status_type_id INT PRIMARY KEY,
+                                   action_type VARCHAR(100) NOT NULL,
+                                   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- ユーザーの作業履歴を見るテーブル
 CREATE TABLE attendances (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    attendance_type INT,
-    start_time TIMESTAMP,
-    end_time TIMESTAMP,
-    date VARCHAR(15),
-    year INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (attendance_type) REFERENCES attendance_types(attendance_type_id)
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             user_id INT,
+                             attendance_type INT,
+                             start_time TIMESTAMP,
+                             end_time TIMESTAMP,
+                             date VARCHAR(15),
+                             year INT,
+                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                             FOREIGN KEY (user_id) REFERENCES users(id),
+                             FOREIGN KEY (attendance_type) REFERENCES attendance_types(user_status_type_id)
+);
+
+-- Userの現在の状態を管理するテーブル
+CREATE TABLE user_statuses (
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             user_id INT,
+                             status_id INT,
+                             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                             FOREIGN KEY (user_id) REFERENCES users(id),
+                             FOREIGN KEY (status_id) REFERENCES user_status_types (attendance_type_id)
 );
 
 -- Attendance_typesテーブルにデータをINSERT
 INSERT INTO attendance_types (attendance_type_id, action_type) VALUES
-(1, '作業'),
-(2, '休憩'),
-(3, 'お祈り');
+                                                                   (1, '作業'),
+                                                                   (2, '休憩'),
+                                                                   (3, 'お祈り');
+
+-- user_statusテーブルにデータをINSERT
+INSERT INTO user_status_types (user_status_type_id, action_type) VALUES
+                                                                   (1, '作業中'),
+                                                                   (2, '休憩中'),
+                                                                   (3, '終了');
